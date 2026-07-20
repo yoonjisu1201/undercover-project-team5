53563e673235ee58ac0de7a1e543c12554424c63
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-20 09:43:06 +0900

    feat(#102) : Chair와 Interact 했을 때 InventoryUI 비활성화하도록 변경. Chair의 커서 활성화, 비활성화 로직 변경, PlayerMoveSample.cs 내부 불필요 함수 삭제,

--- Assets/Scripts/HQ/Chair.cs
+++ Assets/Scripts/HQ/Chair.cs
@@ -3,6 +3,8 @@
 public class Chair : InteractableBase {
 	[Header("=== 의자에 앉았을 때 등장할 콘솔창 ===")]
 	[SerializeField] private MonitorController _consoleUi;
+	[Header("=== 의자에 앉았을 때 UI 꺼주기 위함 ===")]
+	[SerializeField] private InventoryUI _inventoryUI;
 	public override string InteractionText => "앉기";
 
 	public override bool CanInteract => _occupiedPlayer == null;
@@ -15,10 +17,9 @@ public class Chair : InteractableBase {
 
 		// 플레이어의 움직임 막기(콘솔 조작 동안)
 		_occupiedPlayer = interactor.GetComponent<PlayerMoveSample>();
-		_occupiedPlayer.SetActionEnableState(false);
-		Cursor.lockState = CursorLockMode.None;
-		Cursor.visible = true;
-
+		
+		GameplayUiMode.Instance.ActivateCursor();
+		_inventoryUI.gameObject.SetActive(false);
 		_consoleUi.gameObject.SetActive(true);
 		_consoleUi.OnScreenClosed += UnOccupiedPlayer;
 	}
@@ -27,9 +28,8 @@ public class Chair : InteractableBase {
 		_consoleUi.OnScreenClosed -= UnOccupiedPlayer;
 		
 		// 콘솔 조작 끝나면 다시 열어주기
-		_occupiedPlayer.SetActionEnableState(true);
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
+		GameplayUiMode.Instance.DeactivateCursor();
+		_inventoryUI.gameObject.SetActive(true);
 		_occupiedPlayer = null;
 	}
 }
