00fa47796a56baef994e41cf56d943be842b09a7
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-22 13:30:24 +0900

    feat(#106) : 레이어 코드 주석 추가

--- Assets/Scripts/Constants/Layers.cs
+++ Assets/Scripts/Constants/Layers.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 
 public static class Layers {
+	// 카메라에서 특정 레이어 보이지 않도록 처리
 	public static void HideLayerFromCamera(Camera camera, int layerIndex) {
 		camera.cullingMask &= ~(1 << layerIndex);
 	}
-	
+
+	// 카메라에서 특정 레이어 보이도록 처리
 	public static void ShowLayerToCamera(Camera camera, int layerIndex) {
 		camera.cullingMask |= 1 << layerIndex;
 	}
