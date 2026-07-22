438a692d7b9075b17538706bbf9ba750779e61b9
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-22 12:04:26 +0900

    feat(#106) : 미니맵 마커 회전 고정 유틸(KeepWorldRotation) 추가

---
+++ Assets/Scripts/Util/KeepWorldRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+// 미니맵 마커가 플레이어와 함께 회전하지 않도록 하기 위해 추가한 클래스
+// 이외에도 회전 고정을 원하는 오브젝트가 있다면 추가하면 됨.
+public sealed class KeepWorldRotation : MonoBehaviour {
+	private Quaternion _initialWorldRotation;
+
+	private void Awake()
+	{
+		_initialWorldRotation = transform.rotation;
+	}
+
+	private void LateUpdate()
+	{
+		transform.rotation = _initialWorldRotation;
+	}
+}
