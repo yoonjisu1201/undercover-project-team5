c76ab8cdfe48ab98e04401172b5f347062766660
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-22 12:04:37 +0900

    feat(#106) : Player 이름을 NetworkVariable로 동기화하고 미니맵 마커에 표시

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMoveSample), typeof(PlayerInventory), typeof(PlayerInteraction))]
+public class Player : NetworkBehaviour {
+	// 외부에서 Player를 기반으로 Player 검색 후 바로 필요한 컴포넌트 찾아갈 수 있도록
+	[HideInInspector] public PlayerMoveSample PlayerMove;
+	[HideInInspector] public PlayerInventory PlayerInventory;
+	[HideInInspector] public PlayerInteraction PlayerInteraction;
+	[HideInInspector] public PlayerNamePresenter PlayerNamePresenter;
+	
+	private bool _isNetworkStarted => NetworkManager != null && NetworkManager.Singleton.IsListening;
+	
+	private readonly NetworkVariable<FixedString32Bytes> _playerName = new NetworkVariable<FixedString32Bytes>(
+		null,
+		NetworkVariableReadPermission.Everyone,
+		NetworkVariableWritePermission.Owner
+	);
+	
+	private void Awake() {
+		PlayerMove = GetComponent<PlayerMoveSample>();
+		PlayerInventory = GetComponent<PlayerInventory>();
+		PlayerInteraction = GetComponent<PlayerInteraction>();
+		PlayerNamePresenter = GetComponent<PlayerNamePresenter>();
+
+		Layers.HideLayerFromCamera(GetComponentInChildren<Camera>(), Layers.MinimapOnly);
+	}
+
+	public override void OnNetworkSpawn() {
+		_playerName.OnValueChanged += PlayerNamePresenter.HandlePlayerNameChanged;
+		
+		PlayerNamePresenter.HandlePlayerNameChanged(null, PlayerName);
+	}
+	
+	public string PlayerName {
+		get {
+			if (!_isNetworkStarted) { throw new InvalidOperationException($"[Player] 네트워크에 연결되지 않았는데 PlayerName을 요청했습니다."); }
+			if (_playerName.Value != null) { return _playerName.Value.ToString(); }
+			return $"Player {OwnerClientId + 1}";
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerNamePresenter.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNamePresenter : MonoBehaviour {
+	[Header("=== 미니맵에 표시될 플레이어 이름 ===")]
+	[SerializeField] private TMP_Text _minimapNameText;
+
+	[Header("=== 미니맵 마커 ===")] 
+	[SerializeField] private Image _minimapMarker;
+
+	// 플레이어 카메라에선 안 보이게 만들어주기
+	private void Awake() {
+		_minimapNameText.gameObject.layer = Layers.MinimapOnly;
+		_minimapMarker.gameObject.layer = Layers.MinimapOnly; 
+	}
+
+	public void HandlePlayerNameChanged(FixedString32Bytes oldName, FixedString32Bytes newName) {
+		_minimapNameText.text = newName.ToString();
+	}
+}
