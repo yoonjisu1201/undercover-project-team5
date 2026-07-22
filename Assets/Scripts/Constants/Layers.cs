e2ede4ab6ddf908b0aa789cec59634d3e9b3e71c
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-22 11:53:31 +0900

    feat(#106) : 미니맵 전용 레이어(MinimapOnly) 및 레이어 마스크 유틸 추가

---
+++ Assets/Scripts/Constants/Layers.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class Layers {
+	public static void HideLayerFromCamera(Camera camera, int layerIndex) {
+		camera.cullingMask &= ~(1 << layerIndex);
+	}
+	
+	public static void ShowLayerToCamera(Camera camera, int layerIndex) {
+		camera.cullingMask |= 1 << layerIndex;
+	}
+	
+	public static readonly int MinimapOnly = LayerMask.NameToLayer("MinimapOnly");
+}
