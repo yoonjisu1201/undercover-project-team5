0a0d49069578c058ad72e22253fc742392ef8885
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-24 11:26:30 +0900

    fix(#199) : 몽타주 옷 착용 시 원본 몸 오브젝트 비활성화하여 텍스처 번짐 해결
    원본 몸(맨몸) 오브젝트와 새로 생성되는 옷 오브젝트가 겹쳐 렌더링되면서
    경계 부위에 반점(텍스처 번짐)이 발생하던 문제. 옷을 입힐 때 해당 부위의
    원본 Root 오브젝트를 비활성화하고, 벗으면 다시 활성화하도록 처리.

--- Assets/Scripts/HQ/Screen/Montage/Montage.cs
+++ Assets/Scripts/HQ/Screen/Montage/Montage.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class Montage : MonoBehaviour {
 	private Dictionary<MontageParts, GameObject> _clothByParts;
+	private Dictionary<MontageParts, GameObject> _rootParts;
+
+	[Header("=== 각 위치의 루트 아이템들 ===")]
+	[SerializeField] private GameObject HeadRoot;
+	[SerializeField] private GameObject TorsoRoot;
+	[SerializeField] private GameObject ArmRoot;
+	[SerializeField] private GameObject PantsRoot;
+	[SerializeField] private GameObject ShoesRoot;
 
 	[Header("=== 옷을 생성할 부모 Transform ===")]
 	[SerializeField] private Transform _clothParent;
 	public void Initialize() {
 		_clothByParts = new Dictionary<MontageParts, GameObject>();
+		_rootParts = new Dictionary<MontageParts, GameObject>();
+		
+		// 옷을 입히면 사라져야 하는 부위들
+		_rootParts.Add(MontageParts.Torso, TorsoRoot);
+		_rootParts.Add(MontageParts.Arms, ArmRoot);
+		_rootParts.Add(MontageParts.Pants, PantsRoot);
+		_rootParts.Add(MontageParts.Shoes, ShoesRoot);
 	}
 	
 	// 옷입히기
@@ -16,8 +30,14 @@ public class Montage : MonoBehaviour {
 		// 이미 존재하면 갈아입혀야 함.
 		if (_clothByParts.TryGetValue(part, out _)) { RemoveCloth(part); }
 		
+		
 		GameObject obj = Instantiate(cloth, transform);
 		obj.transform.localPosition = Vector3.zero;
+		
+		// 옷을 입으면, 그 부위의 RootObject를 비활성화
+		if (_rootParts.TryGetValue(part, out GameObject rootObj)) {
+			rootObj.gameObject.SetActive(false);
+		}
 		
 		_clothByParts[part] = obj;
 	}
@@ -30,5 +50,10 @@ public class Montage : MonoBehaviour {
 		
 		Destroy(_clothByParts[part]);
 		_clothByParts[part] = null;
+		
+		// 옷을 벗으면, 그 부위의 RootObject를 활성화
+		if (_rootParts.TryGetValue(part, out GameObject rootObj)) {
+			rootObj.gameObject.SetActive(true);
+		}
 	}
 }
