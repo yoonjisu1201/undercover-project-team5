bf588caec80cc593d527a7301fd49347e641621e
hyunwoonam-digipen <hyunwoo.nam@digipen.edu>
2026-07-27 16:57:04 +0900

    fix(glitch #280): 글리치 효과 변경

--- Assets/Scripts/InterferenceEffect/GlitchInterferenceEffect.cs
+++ Assets/Scripts/InterferenceEffect/GlitchInterferenceEffect.cs
@@ -1,4 +1,5 @@
 // 플레이어의 시야를 방해하는 효과를 식별합니다.
+using GlitchSample;
 using Unity.Netcode;
 using Unity.Services.Matchmaker.Models;
 using Unity.VisualScripting;
@@ -11,6 +12,7 @@ public sealed class GlitchInterferenceEffect : InterferenceEffectBase
     [SerializeField] private Volume _globalVolume;
 
     private AnalogGlitchVolume _analogGlitch;
+    private BlockGlitchVolume _blockGlitch;
 
     // 시야 방해 효과 식별자를 가져옵니다.
     public override InterferenceEffectType _type => InterferenceEffectType.Glitch;
@@ -18,11 +20,13 @@ public sealed class GlitchInterferenceEffect : InterferenceEffectBase
     private void Awake()
     {
         _globalVolume.profile.TryGet<AnalogGlitchVolume>(out _analogGlitch);
+        _globalVolume.profile.TryGet<BlockGlitchVolume>(out _blockGlitch);
     }
 
     public override void Activate()
     {
         _analogGlitch.active = true;
+        _blockGlitch.active = true;
 
         base.Activate();
     }
@@ -30,6 +34,7 @@ public sealed class GlitchInterferenceEffect : InterferenceEffectBase
     public override void Deactivate(InterferenceEndReason reason)
     {
         _analogGlitch.active = false;
+        _blockGlitch.active = false;
 
         base.Deactivate(reason);
     }
