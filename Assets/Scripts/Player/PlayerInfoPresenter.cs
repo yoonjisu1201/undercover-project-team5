690d9de96d8d31fcc1c96c2bef7c1f241ec99c36
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-07-29 13:54:51 +0900

    feat(#190) : 인게임 캐릭터 닉네임 태그 표시

--- Assets/Scripts/Player/PlayerInfoPresenter.cs
+++ Assets/Scripts/Player/PlayerInfoPresenter.cs
@@ -10,14 +10,22 @@ public class PlayerInfoPresenter : MonoBehaviour {
 	[Header("=== 미니맵 마커 ===")] 
 	[SerializeField] private Image _minimapMarker;
 
+	[Header("=== 캐릭터의 머리 위에 뜨는 닉네임마커 ===")] 
+	[SerializeField] private PlayerNameTag _nameTag;
+	
+	private Player _player;
+
 	// 플레이어 카메라에서는 미니맵 마커, 미니맵 이름표 안 보이게 만들어주기
 	private void Awake() {
 		_minimapNameText.gameObject.layer = Layers.MinimapOnly;
 		_minimapMarker.gameObject.layer = Layers.MinimapOnly;
 	}
+	
 
+	// 닉네임 변경 시 MinimapText, 머리위 닉네임마커 값 수정
 	public void HandlePlayerNameChanged(FixedString32Bytes oldName, FixedString32Bytes newName) {
 		_minimapNameText.text = newName.ToString();
+		_nameTag.SetText(newName.ToString());
 	}
 
 	public void HandlePlayerColorChanged(Color colorBefore, Color colorAfter) {
---
+++ Assets/Scripts/Player/PlayerNameTag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class PlayerNameTag : MonoBehaviour {
+	[Header("=== 이름표 텍스트 등록 ===")]
+	[SerializeField] private TMP_Text _nameText;
+
+	// LocalCamera에서만 보이도록 함. 미니맵, CCTV에서는 닉네임 확인 안되게
+	private void Awake() {
+		_nameText.gameObject.layer = Layers.LocalCameraOnly;
+	}
+
+	public void SetText(string text) {
+		_nameText.text = text;
+	}
+
+	private void LateUpdate() {
+		Camera mainCamera = LocalCameraProvider.MainCamera;
+		
+		// 메인카메라 지정되기 전에는 안함
+		if (mainCamera == null) { return; }
+		
+		// x회전은 적용 안시킴. 적용시키면 하늘 봤을 때 이름표도 같이 하늘 봄..
+		Quaternion newRotation = mainCamera.transform.rotation;
+		newRotation.x = 0f;
+		transform.rotation = newRotation;
+	}
+}
