d1c0b0521622a914148f66d5735145cd69bfdacc
정호종 <33508056+LteFroggy@users.noreply.github.com>
2026-08-13 14:53:39 +0900

    feat(#461) : 가이드북 튜토리얼 페이지 및 몽타주 안내 보강

---
+++ Assets/Scripts/UI/GuideBook/GuideBookPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class GuideBookPage : MonoBehaviour {
+	[Header("=== 가이드북 내부 요소 ===")]
+	[SerializeField] private TMP_Text _headerText;
+	[SerializeField] private TMP_Text _subtitleText;
+	[SerializeField] private TMP_Text _sectionNumber;
+	[SerializeField] private TMP_Text _pageIndicatorText;
+	
+	private RectTransform _rectTransform;
+	public RectTransform RectTransform => _rectTransform ??= GetComponent<RectTransform>();
+
+	public void Initialize(
+		string headerText,
+		string subtitleText,
+		uint pageNum, 
+		uint totalPageNumber) 
+	{
+		if (_pageIndicatorText != null) _pageIndicatorText.text = $"{pageNum:D2} / {totalPageNumber:D2}";
+		if (_sectionNumber != null) _sectionNumber.text = $"{pageNum:D2}";
+		if (_headerText != null) _headerText.text = headerText;
+		if (_subtitleText != null) _subtitleText.text = subtitleText;
+	}
+}
